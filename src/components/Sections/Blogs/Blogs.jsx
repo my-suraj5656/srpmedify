c298c057f99034db5c62382d5062726cb7bc7ef7
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-12-18 20:40:08 +0530

    BlogCard and Blogs added

--- src/components/Sections/Blogs/Blogs.jsx
+++ src/components/Sections/Blogs/Blogs.jsx
@@ -2,29 +2,31 @@ import { Box, Container, Grid, Typography } from "@mui/material";
 import BlogCard from "./BlogCard";
 
 export default function Blogs() {
-    return (
-        <Box py={6}>
-            <Container>
-                <Typography color='primary.main' fontWeight={600} textAlign='center'>
-                    Blog & News
-                </Typography>
-                <Typography textAlign='center' variant='h2' mb={2}>
-                    Read Our Latest News
-                </Typography>
+  const blogTitleStyle = {
+    color: "primary.main",
+    fontWeight: 600,
+    textAlign: "center",
+  };
 
-                <Grid container spacing={4}>
-                    <Grid item xs={12} md={4}>
-                        <BlogCard />
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <BlogCard />
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <BlogCard />
-                    </Grid>
-                </Grid>
+  const blogSubtitleStyle = {
+    textAlign: "center",
+    variant: "h2",
+    mb: 2,
+  };
 
-            </Container>
-        </Box>
-    )
+  return (
+    <Box py={6}>
+      <Container>
+        <Typography sx={blogTitleStyle}>Blog & News</Typography>
+        <Typography sx={blogSubtitleStyle}>Read Our Latest News</Typography>
+        <Grid container spacing={4}>
+          {[1, 2, 3].map((_, index) => (
+            <Grid item xs={12} md={4} key={index}>
+              <BlogCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
